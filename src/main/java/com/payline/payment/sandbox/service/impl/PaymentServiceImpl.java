e8a7bf6aceebcb7223928c45de5a5fde5334df4a
Martin <martin.grescu@mythalesgroup.io>
2020-03-09 11:09:12 +0100

    PAYLAPMEXT-207: En cours

--- src/main/java/com/payline/payment/sandbox/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/payline/payment/sandbox/service/impl/PaymentServiceImpl.java
@@ -11,6 +11,10 @@ import com.payline.pmapi.bean.payment.response.impl.PaymentResponseFormUpdated;
 import com.payline.pmapi.bean.payment.response.impl.PaymentResponseRedirect;
 import com.payline.pmapi.bean.payment.response.impl.PaymentResponseSuccess;
 import com.payline.pmapi.bean.paymentform.bean.field.PaymentFormField;
+import com.payline.pmapi.bean.paymentform.bean.field.PaymentFormInputFieldCheckbox;
+import com.payline.pmapi.bean.paymentform.bean.field.PaymentFormInputFieldSelect;
+import com.payline.pmapi.bean.paymentform.bean.field.PaymentFormInputFieldText;
+import com.payline.pmapi.bean.paymentform.bean.field.specific.*;
 import com.payline.pmapi.bean.paymentform.bean.form.CustomForm;
 import com.payline.pmapi.bean.paymentform.bean.form.PartnerWidgetForm;
 import com.payline.pmapi.bean.paymentform.bean.form.partnerwidget.PartnerWidgetOnPay;
@@ -31,6 +35,9 @@ import java.util.stream.Collectors;
 
 public class PaymentServiceImpl extends AbstractService<PaymentResponse> implements PaymentService {
 
+    private CustomForm customForm;
+    private boolean isPaymentFormUpdatedFilled = false;
+    /**------------------------------------------------------------------------------------------------------------------*/
     @Override
     public PaymentResponse paymentRequest(PaymentRequest paymentRequest) {
         this.verifyRequest(paymentRequest);
@@ -38,46 +45,52 @@ public class PaymentServiceImpl extends AbstractService<PaymentResponse> impleme
         String amount = paymentRequest.getAmount().getAmountInSmallestUnit().toString();
 
         /* PaymentResponseSuccess */
-        if( "10000".equals( amount )
-                || amount.matches("^[3-9][0-9]*$") ){
+        if ("10000".equals(amount) || amount.matches("^[3-9][0-9]*$") || ("10300".equals(amount) && isPaymentFormUpdatedFilled)) {
             // TODO: manage the case where the amount is "103XX" and the form has been updated (PAYLAPMEXT-207)
+            String transactionAdditionalData = new String();
+
             PaymentResponseSuccess.PaymentResponseSuccessBuilder builder = PaymentResponseSuccess.PaymentResponseSuccessBuilder.aPaymentResponseSuccess()
-                    .withPartnerTransactionId( PaymentResponseUtil.PARTNER_TRANSACTION_ID )
-                    .withTransactionDetails( new EmptyTransactionDetails() );
+                    .withPartnerTransactionId(PaymentResponseUtil.PARTNER_TRANSACTION_ID)
+                    .withTransactionDetails(new EmptyTransactionDetails());
 
             // If the payment form contains data, put them into transaction additional data
-            if( paymentRequest.getPaymentFormContext() != null
+            if (paymentRequest.getPaymentFormContext() != null
                     && paymentRequest.getPaymentFormContext().getPaymentFormParameter() != null
-                    && !paymentRequest.getPaymentFormContext().getPaymentFormParameter().isEmpty() ){
-                String transactionAdditionalData = paymentRequest.getPaymentFormContext().getPaymentFormParameter()
+                    && !paymentRequest.getPaymentFormContext().getPaymentFormParameter().isEmpty()) {
+
+                transactionAdditionalData = paymentRequest.getPaymentFormContext().getPaymentFormParameter()
                         .entrySet().stream()
                         .map(e -> e.getKey() + "=" + e.getValue())
-                        .collect(Collectors.joining( "&" ));
+                        .collect(Collectors.joining("&"));
 
                 transactionAdditionalData = transactionAdditionalData + "&" + paymentRequest.getPaymentFormContext().getSensitivePaymentFormParameter()
                         .entrySet().stream()
                         .map(e -> e.getKey() + "=" + e.getValue())
-                        .collect(Collectors.joining( "&" ));
-
-
-                builder.withTransactionAdditionalData( transactionAdditionalData );
-
+                        .collect(Collectors.joining("&"));
             }
+            if ("10300".equals(amount) && isPaymentFormUpdatedFilled) {
+                // TODO : Ajouter les éléments du PaymentFormUpdated aux transactionAdditionalData si l'on est dans le cas 10300 et que le formulaire est déjà rempli
+                transactionAdditionalData = transactionAdditionalData + getformatedStringFromCustomFormData();
+                System.out.println(transactionAdditionalData);
+            }
+
+
+            builder.withTransactionAdditionalData(transactionAdditionalData);
 
             return builder.build();
         }
 
         /* PaymentResponseFailure */
-        if( "10100".equals( amount ) ){
+        if ("10100".equals(amount)) {
             return PaymentResponseUtil.failureClassic();
         }
-        if( "10101".equals( amount ) ){
+        if ("10101".equals(amount)) {
             return PaymentResponseUtil.failureMinimal();
         }
-        if( "10102".equals( amount ) ){
+        if ("10102".equals(amount)) {
             return PaymentResponseUtil.failureLongErrorCode();
         }
-        if( "10103".equals( amount ) ){
+        if ("10103".equals(amount)) {
             return PaymentResponseUtil.failureWithPartnerTransactionId();
         }
 
@@ -86,68 +99,96 @@ public class PaymentServiceImpl extends AbstractService<PaymentResponse> impleme
         try {
             redirectionRequest = PaymentResponseRedirect.RedirectionRequest.RedirectionRequestBuilder.aRedirectionRequest()
                     .withRequestType(PaymentResponseRedirect.RedirectionRequest.RequestType.GET)
-                    .withUrl( new URL("https", "www.google.com", "/fr") )
+                    .withUrl(new URL("https", "www.google.com", "/fr"))
                     .build();
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        if( "10200".equals( amount ) || amount.startsWith("2") ){
+        if ("10200".equals(amount) || amount.startsWith("2")) {
             return PaymentResponseRedirect.PaymentResponseRedirectBuilder.aPaymentResponseRedirect()
-                    .withRedirectionRequest( redirectionRequest )
-                    .withPartnerTransactionId( PaymentResponseUtil.PARTNER_TRANSACTION_ID )
+                    .withRedirectionRequest(redirectionRequest)
+                    .withPartnerTransactionId(PaymentResponseUtil.PARTNER_TRANSACTION_ID)
                     .build();
         }
-        if( "10201".equals( amount ) ){
+        if ("10201".equals(amount)) {
             Map<String, String> context = new HashMap<>();
             context.put("key", "value");
             return PaymentResponseRedirect.PaymentResponseRedirectBuilder.aPaymentResponseRedirect()
-                    .withRedirectionRequest( redirectionRequest )
-                    .withPartnerTransactionId( PaymentResponseUtil.PARTNER_TRANSACTION_ID )
+                    .withRedirectionRequest(redirectionRequest)
+                    .withPartnerTransactionId(PaymentResponseUtil.PARTNER_TRANSACTION_ID)
                     .withStatusCode("PARTNER_STATUS")
                     .withRequestContext(
                             RequestContext.RequestContextBuilder.aRequestContext()
-                            .withRequestData(context)
-                            .build()
+                                    .withRequestData(context)
+                                    .build()
                     )
                     .build();
         }
 
         /* PaymentResponseFormUpdated */
         // TODO : PAYLAPMEXT-207
-        if( "10300".equals( amount ) ){
+        if ("10300".equals(amount)) {
+            try {
+                this.customForm = PaymentResponseUtil.aCustomForm();
+                this.isPaymentFormUpdatedFilled = true;
+
+                // Create the PaymentFormConfigurationResponse
+                PaymentFormConfigurationResponse paymentFormConfigurationResponse = PaymentFormConfigurationResponseSpecific
+                        .PaymentFormConfigurationResponseSpecificBuilder
+                        .aPaymentFormConfigurationResponseSpecific()
+                        .withPaymentForm(customForm)
+                        .build();
+
+                Map<String, String> context = new HashMap<>();
+                context.put("key13", "value");
+
+                // Return the PaymentResponseFormUpdated
+                return PaymentResponseFormUpdated.PaymentResponseFormUpdatedBuilder
+                        .aPaymentResponseFormUpdated()
+                        .withPaymentFormConfigurationResponse(paymentFormConfigurationResponse)
+                        .withRequestContext(RequestContext.RequestContextBuilder.aRequestContext()
+                                .withRequestData(context)
+                                .build())
+                        .build();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            //
 
         }
 
+
         /* PaymentResponseDoPayment */
-        if( "10400".equals( amount ) ){
+        if ("10400".equals(amount)) {
             return PaymentResponseUtil.doPaymentMinimal();
         }
 
         /* PaymentResponseActiveWaiting */
-        if( "10500".equals( amount ) ){
+        if ("10500".equals(amount)) {
             return new PaymentResponseActiveWaiting();
         }
 
         /* Generic plugin behaviours */
-        return super.generic( amount );
+        return super.generic(amount);
     }
-
+    /**------------------------------------------------------------------------------------------------------------------*/
     /**
      * Performs standard verification of the request content.
      * Checks that every field required to process to a payment is filled in the request
      * (ex: PartnerConfiguration, Amount, etc.)
+     *
      * @param paymentRequest
      */
     private void verifyRequest(PaymentRequest paymentRequest) {
-        if( paymentRequest.getContractConfiguration() == null
+        if (paymentRequest.getContractConfiguration() == null
                 || paymentRequest.getPartnerConfiguration() == null
-                || paymentRequest.getAmount() == null ){
-            throw new IllegalArgumentException( "The PaymentRequest is missing required data" );
+                || paymentRequest.getAmount() == null) {
+            throw new IllegalArgumentException("The PaymentRequest is missing required data");
         }
     }
 
+    /**------------------------------------------------------------------------------------------------------------------*/
     /**
-     *
      * @param paymentRequest
      * @return
      */
@@ -236,5 +277,103 @@ public class PaymentServiceImpl extends AbstractService<PaymentResponse> impleme
         return null;
 
     }
+    /**------------------------------------------------------------------------------------------------------------------*/
+    /**
+     * Return a formated string with the customForm data
+     * @return
+     */
+    private String getformatedStringFromCustomFormData() {
+
+        String transactionAdditionalData = new String();
+
+        for (PaymentFormField paymentFormField : this.customForm.getCustomFields()) {
+
+            if (!transactionAdditionalData.isEmpty()) {
+                transactionAdditionalData = transactionAdditionalData + "&";
+            }
+
+            // PaymentFormInputFieldAmount
+            if (paymentFormField instanceof PaymentFormInputFieldAmount) {
+                transactionAdditionalData = transactionAdditionalData
+                        + ((PaymentFormInputFieldAmount) paymentFormField).getKey()
+                        + "="
+                        + ((PaymentFormInputFieldAmount) paymentFormField).getValue();
+            }
+
+            // PaymentFormInputFieldBic
+            if (paymentFormField instanceof PaymentFormInputFieldBic) {
+                transactionAdditionalData = transactionAdditionalData
+                        + ((PaymentFormInputFieldBic) paymentFormField).getKey()
+                        + "="
+                        + ((PaymentFormInputFieldBic) paymentFormField).getLabel();
+            }
+            // PaymentFormInputFieldBirthdate
+            if (paymentFormField instanceof PaymentFormInputFieldBirthdate) {
+                transactionAdditionalData = transactionAdditionalData
+                        + ((PaymentFormInputFieldBirthdate) paymentFormField).getKey()
+                        + "="
+                        + ((PaymentFormInputFieldBirthdate) paymentFormField).getLabel();
+            }
+            // PaymentFormInputFieldCardNumber
+            if (paymentFormField instanceof PaymentFormInputFieldCardNumber) {
+                transactionAdditionalData = transactionAdditionalData
+                        + ((PaymentFormInputFieldCardNumber) paymentFormField).getKey()
+                        + "="
+                        + ((PaymentFormInputFieldCardNumber) paymentFormField).getLabel();
+            }
+            // PaymentFormInputFieldCardholder
+            if (paymentFormField instanceof PaymentFormInputFieldCardholder) {
+                transactionAdditionalData = transactionAdditionalData
+                        + ((PaymentFormInputFieldCardholder) paymentFormField).getKey()
+                        + "="
+                        + ((PaymentFormInputFieldCardholder) paymentFormField).getLabel();
+            }
+            // PaymentFormInputFieldCheckbox
+            if (paymentFormField instanceof PaymentFormInputFieldCheckbox) {
+                transactionAdditionalData = transactionAdditionalData
+                        + ((PaymentFormInputFieldCheckbox) paymentFormField).getKey()
+                        + "="
+                        + ((PaymentFormInputFieldCheckbox) paymentFormField).getLabel();
+            }
+            // PaymentFormInputFieldCvx
+            if (paymentFormField instanceof PaymentFormInputFieldCvx) {
+                transactionAdditionalData = transactionAdditionalData
+                        + ((PaymentFormInputFieldCvx) paymentFormField).getKey()
+                        + "="
+                        + ((PaymentFormInputFieldCvx) paymentFormField).getLabel();
+            }
+            // PaymentFormInputFieldExpirationDate
+            if (paymentFormField instanceof PaymentFormInputFieldExpirationDate) {
+                transactionAdditionalData = transactionAdditionalData
+                        + ((PaymentFormInputFieldExpirationDate) paymentFormField).getKey()
+                        + "="
+                        + ((PaymentFormInputFieldExpirationDate) paymentFormField).getLabel();
+            }
+            // PaymentFormInputFieldIban
+            if (paymentFormField instanceof PaymentFormInputFieldIban) {
+                transactionAdditionalData = transactionAdditionalData
+                        + ((PaymentFormInputFieldIban) paymentFormField).getKey()
+                        + "="
+                        + ((PaymentFormInputFieldIban) paymentFormField).getLabel();
+            }
+            // PaymentFormInputFieldSelect
+            if (paymentFormField instanceof PaymentFormInputFieldSelect) {
+                transactionAdditionalData = transactionAdditionalData
+                        + ((PaymentFormInputFieldSelect) paymentFormField).getKey()
+                        + "="
+                        + ((PaymentFormInputFieldSelect) paymentFormField).getLabel();
+            }
+            // PaymentFormInputFieldText
+            if (paymentFormField instanceof PaymentFormInputFieldText) {
+                transactionAdditionalData = transactionAdditionalData
+                        + ((PaymentFormInputFieldText) paymentFormField).getKey()
+                        + "="
+                        + ((PaymentFormInputFieldText) paymentFormField).getValue();
+            }
+
+        }
+        return transactionAdditionalData;
+    }
+    /**------------------------------------------------------------------------------------------------------------------*/
 
 }
